e48f250a5b27226ce26aedddc4c76b30d6a12c43
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-01-05 15:35:46 +0100

    add styling to Reviews

--- next/src/components/sections/Reviews/index.js
+++ next/src/components/sections/Reviews/index.js
@@ -7,8 +7,8 @@ const Reviews = ({
   list,
 }) => {
   list = list.map(({ img, content, ...props }) => ({
-    img: <Img data={img} />,
-    content: <Markdown>{content}</Markdown>,
+    img: img ? <Img data={img} sizes='64px' /> : <UserPlaceholder />,
+    content: <Markdown className={styles.content}>{content}</Markdown>,
     ...props
   }));
   
@@ -20,9 +20,15 @@ const Reviews = ({
       <ul className={styles.list}>
         {list.map(({ img, name, position, content }, i) => (
           <li className={styles.item} key={i}>
-            {img}
-            <p>{name}</p>
-            <p>{position}</p>
+            <div className={styles.author}>
+              <div className={styles.img}>
+                {img}
+              </div>
+              <div>
+                <h3>{name}</h3>
+                <p className={styles.position}>{position}</p>
+              </div>
+            </div>
             {content}
           </li>
         ))}
@@ -32,3 +38,29 @@ const Reviews = ({
 };
 
 export default Reviews;
+
+const UserPlaceholder = ({ ...props }) => (
+  <svg xmlns='http://www.w3.org/2000/svg' width='44' height='50' viewBox='0 0 44 50' fill='none' {...props}>
+    <path
+      stroke='#282E4B'
+      d='M41.749 5.039c-17.765-7.315-33.581-3.048-39.269 0C-4.105 35.355 12.826 46.978 22.114 49 44.475 43.303 44.522 17.319 41.75 5.039z'
+    ></path>
+    <ellipse
+      cx='21.997'
+      cy='17.317'
+      stroke='#53BAFD'
+      strokeWidth='1.5'
+      rx='4.995'
+      ry='5.122'
+    ></ellipse>
+    <ellipse
+      cx='21.997'
+      cy='31.403'
+      stroke='#53BAFD'
+      strokeWidth='1.5'
+      opacity='0.5'
+      rx='8.741'
+      ry='5.122'
+    ></ellipse>
+  </svg>
+);
