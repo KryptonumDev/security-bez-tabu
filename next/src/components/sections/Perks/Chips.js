1dc8b9db423addf8080e4c0815e87a2a11560ebe
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-01-09 14:42:06 +0100

    improved Chips animation in Perks components

--- next/src/components/sections/Perks/Chips.js
+++ next/src/components/sections/Perks/Chips.js
@@ -3,6 +3,23 @@ import { useRef } from 'react';
 import styles from './styles.module.scss';
 import { motion, useInView } from 'framer-motion';
 
+const randomDurations = Array.from({ length: 8 }).map(() => (4 + Math.random() * 4));
+const MotionRect = (isInView, index=0) => ({
+  initial: { opacity: 0 },
+  animate: { opacity: isInView ? 1 : 0 },
+  transition: {
+    delay: randomDurations[index] * .35,
+  },
+})
+const MotionPath = (isInView, index) => ({
+  initial: { pathLength: 0 },
+  animate: { pathLength: isInView ? 1 : 0 },
+  transition: {
+    type: "spring",
+    duration: randomDurations[index]
+  },
+})
+
 const Chips = () => {
   const wrapper = useRef(null);
   const isInView = useInView(wrapper, { once: true, margin: "0px 0px -5% 0px" });
@@ -17,191 +34,142 @@ const Chips = () => {
 
 export default Chips;
 
-const Chip = ({ filled, isInView, ...props }) => (
+const Chip = ({ filled, isInView }) => (
   <svg
     xmlns='http://www.w3.org/2000/svg'
     width='420'
     height='431'
-    fill='none'
     viewBox='0 0 420 431'
+    fill='none'
     stroke={`${filled ? 'var(--secondary-500)' : 'var(--primary-700)'}`}
-    {...props}
   >
-    <motion.path
-      d='M8 164.799h173l56.5-56.5H318M8 217.799h173l56.5-56.5H318'
-      {...filled && {
-        initial: { pathLength: 0 },
-        animate: { pathLength: isInView ? 1 : 0 },
-        transition: {
-          type: "spring",
-          duration: 8,
-        },
-      }}
-    ></motion.path>
-    <motion.path
-      d='M8 118.799h152m-152 148h102l57 38h245m-404 13h92l63 48h94M8 65.299h92l63-48h94M8 374.799h92l63 48h94M8 8.799h80'
-      {...filled && {
-        initial: { pathLength: 0 },
-        animate: { pathLength: isInView ? 1 : 0 },
-        transition: {
-          type: "spring",
-          duration: 8,
-        },
-      }}
-    ></motion.path>
-    <motion.g
-      {...filled && {
-        initial: { opacity: 0 },
-        animate: { opacity: isInView ? 1 : 0 },
-        transition: {
-          delay: 1,
-        },
-      }}
-    >
-      <rect
-        width='15'
-        height='15'
-        x='0.5'
-        y='172.299'
-        rx='7.5'
-        transform='rotate(-90 .5 172.299)'
-        fill={`${filled ? 'var(--secondary-500)' : '#13182A'}`}
-      />
-      <rect
-        width='15'
-        height='15'
-        x='0.5'
-        y='225.299'
-        fill={`${filled ? 'var(--secondary-500)' : '#13182A'}`}
-        rx='7.5'
-        transform='rotate(-90 .5 225.299)'
-      />
-      <rect
-        width='15'
-        height='15'
-        x='0.5'
-        y='274.299'
-        fill={`${filled ? 'var(--secondary-500)' : '#13182A'}`}
-        rx='7.5'
-        transform='rotate(-90 .5 274.299)'
-      />
-      <rect
-        width='15'
-        height='15'
-        x='0.5'
-        y='325.299'
-        fill={`${filled ? 'var(--secondary-500)' : '#13182A'}`}
-        rx='7.5'
-        transform='rotate(-90 .5 325.299)'
-      />
-      <rect
-        width='15'
-        height='15'
-        x='0.5'
-        y='0.5'
-        fill={`${filled ? 'var(--secondary-500)' : '#13182A'}`}
-        rx='7.5'
-        transform='matrix(0 1 1 0 0 57.299)'
-      />
-      <rect
-        width='15'
-        height='15'
-        x='0.5'
-        y='0.5'
-        fill={`${filled ? 'var(--secondary-500)' : '#13182A'}`}
-        rx='7.5'
-        transform='matrix(0 1 1 0 0 .299)'
-      />
-      <rect
-        width='15'
-        height='15'
-        x='0.5'
-        y='0.5'
-        fill={`${filled ? 'var(--secondary-500)' : '#13182A'}`}
-        rx='7.5'
-        transform='matrix(0 1 1 0 80 .299)'
-      />
-      <rect
-        width='15'
-        height='15'
-        x='0.5'
-        y='382.299'
-        fill={`${filled ? 'var(--secondary-500)' : '#13182A'}`}
-        rx='7.5'
-        transform='rotate(-90 .5 382.299)'
-      />
-      <rect
-        width='15'
-        height='15'
-        x='404.5'
-        y='312.299'
-        fill={`${filled ? 'var(--secondary-500)' : '#13182A'}`}
-        rx='7.5'
-        transform='rotate(-90 404.5 312.299)'
-      />
-      <rect
-        width='15'
-        height='15'
-        x='0.5'
-        y='126.299'
-        fill={`${filled ? 'var(--secondary-500)' : '#13182A'}`}
-        rx='7.5'
-        transform='rotate(-90 .5 126.299)'
-      />
-      <rect
-        width='15'
-        height='15'
-        x='307.5'
-        y='115.799'
-        fill={`${filled ? 'var(--secondary-500)' : '#13182A'}`}
-        rx='7.5'
-        transform='rotate(-90 307.5 115.799)'
-      />
-      <rect
-        width='15'
-        height='15'
-        x='307.5'
-        y='168.799'
-        fill={`${filled ? 'var(--secondary-500)' : '#13182A'}`}
-        rx='7.5'
-        transform='rotate(-90 307.5 168.799)'
-      />
-      <rect
-        width='15'
-        height='15'
-        x='160.5'
-        y='126.299'
-        fill={`${filled ? 'var(--secondary-500)' : '#13182A'}`}
-        rx='7.5'
-        transform='rotate(-90 160.5 126.299)'
-      />
-      <rect
-        width='15'
-        height='15'
-        x='257.5'
-        y='373.299'
-        fill={`${filled ? 'var(--secondary-500)' : '#13182A'}`}
-        rx='7.5'
-        transform='rotate(-90 257.5 373.299)'
-      />
-      <rect
-        width='15'
-        height='15'
-        x='0.5'
-        y='0.5'
-        fill={`${filled ? 'var(--secondary-500)' : '#13182A'}`}
-        rx='7.5'
-        transform='matrix(0 1 1 0 257 9.299)'
-      />
-      <rect
+    <g style={{ animationDelay: `${randomDurations[0]}s` }}>
+      <motion.rect
         width='15'
         height='15'
         x='257.5'
         y='430.299'
-        fill={`${filled ? 'var(--secondary-500)' : '#13182A'}`}
         rx='7.5'
         transform='rotate(-90 257.5 430.299)'
+        fill={`${filled ? 'var(--secondary-500)' : '#13182A'}`}
+        {...filled && MotionRect(isInView, 0)}
+        ></motion.rect>
+      <motion.path
+        d='M8 374.799h92l63 48h94'
+        {...filled && MotionPath(isInView, 0)}
       />
-    </motion.g>
+    </g>
+    <g style={{ animationDelay: `${randomDurations[1]}s` }}>
+      <motion.rect
+        width='15'
+        height='15'
+        x='257.5'
+        y='373.299'
+        rx='7.5'
+        transform='rotate(-90 257.5 373.299)'
+        fill={`${filled ? 'var(--secondary-500)' : '#13182A'}`}
+        {...filled && MotionRect(isInView, 1)}
+      ></motion.rect>
+      <motion.path
+        d='M8 317.799h92l63 48h94'
+        {...filled && MotionPath(isInView, 1)}
+      />
+    </g>
+    <g style={{ animationDelay: `${randomDurations[2]}s` }}>
+      <motion.rect
+        width='15'
+        height='15'
+        x='404.5'
+        y='312.299'
+        rx='7.5'
+        transform='rotate(-90 404.5 312.299)'
+        fill={`${filled ? 'var(--secondary-500)' : '#13182A'}`}
+        {...filled && MotionRect(isInView, 2)}
+      ></motion.rect>
+      <motion.path
+        d='M8 266.799h102l57 38h245'
+        {...filled && MotionPath(isInView, 2)}
+      />
+    </g>
+    <g style={{ animationDelay: `${randomDurations[3]}s` }}>
+      <motion.rect
+        width='15'
+        height='15'
+        x='307.5'
+        y='168.799'
+        rx='7.5'
+        transform='rotate(-90 307.5 168.799)'
+        fill={`${filled ? 'var(--secondary-500)' : '#13182A'}`}
+        {...filled && MotionRect(isInView, 3)}
+      ></motion.rect>
+      <motion.path
+        d='M8 217.799h173l56.5-56.5H318'
+        {...filled && MotionPath(isInView, 3)}
+      />
+    </g>
+    <g style={{ animationDelay: `${randomDurations[4]}s` }}>
+      <motion.rect
+        width='15'
+        height='15'
+        x='307.5'
+        y='115.799'
+        rx='7.5'
+        transform='rotate(-90 307.5 115.799)'
+        fill={`${filled ? 'var(--secondary-500)' : '#13182A'}`}
+        {...filled && MotionRect(isInView, 4)}
+      ></motion.rect>
+      <motion.path
+        d='M8 164.799h173l56.5-56.5H318'
+        {...filled && MotionPath(isInView, 4)}
+      />
+    </g>
+    <g style={{ animationDelay: `${randomDurations[5]}s` }}>
+      <motion.rect
+        width='15'
+        height='15'
+        x='160.5'
+        y='126.299'
+        rx='7.5'
+        transform='rotate(-90 160.5 126.299)'
+        fill={`${filled ? 'var(--secondary-500)' : '#13182A'}`}
+        {...filled && MotionRect(isInView, 5)}
+      ></motion.rect>
+      <motion.path
+        d='M8 118.799h152'
+        {...filled && MotionPath(isInView, 5)}
+      />
+    </g>
+    <g style={{ animationDelay: `${randomDurations[6]}s` }}>
+      <motion.rect
+        width='15'
+        height='15'
+        x='0.5'
+        y='0.5'
+        rx='7.5'
+        transform='matrix(0 1 1 0 257 9.299)'
+        fill={`${filled ? 'var(--secondary-500)' : '#13182A'}`}
+        {...filled && MotionRect(isInView, 6)}
+      ></motion.rect>
+      <motion.path
+        d='M8 65.299h92l63-48h94'
+        {...filled && MotionPath(isInView, 6)}
+      />
+    </g>
+    <g style={{ animationDelay: `${randomDurations[7]}s` }}>
+      <motion.rect
+        width='15'
+        height='15'
+        x='0.5'
+        y='0.5'
+        rx='7.5'
+        transform='matrix(0 1 1 0 80 .299)'
+        fill={`${filled ? 'var(--secondary-500)' : '#13182A'}`}
+        {...filled && MotionRect(isInView, 7)}
+      ></motion.rect>
+      <motion.path
+        d='M8 8.799h80'
+        {...filled && MotionPath(isInView, 7)}
+      />
+    </g>
   </svg>
 )
