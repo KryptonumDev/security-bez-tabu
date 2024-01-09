59701055fff8a60053aad9becd398670b7d46828
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-01-09 17:43:14 +0100

    add Linear chart two variants

--- next/src/app/layout.js
+++ next/src/app/layout.js
@@ -50,17 +50,16 @@ export default function RootLayout({ children }) {
             })(window,document,'script','dataLayer','${process.env.GTM_ID}');`}
         </Script>
       </head>
-        <body className={`${Saira.className} ${IBMPlexSansHebrew.variable}`}>
-          <Header />
-          <SmoothScroll>
-            <main id="main">
-              {children}
-            </main>
-          </SmoothScroll>
-          <Footer />
-          <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`}} 
-    />
-        </body>
+      <body className={`${Saira.className} ${IBMPlexSansHebrew.variable}`}>
+        <Header />
+        <SmoothScroll>
+          <main id="main">
+            {children}
+          </main>
+        </SmoothScroll>
+        <Footer />
+        <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`}} />
+      </body>
     </html>
   )
 }
--- next/src/components/sections/Charts/Linear.js
+++ next/src/components/sections/Charts/Linear.js
@@ -7,8 +7,8 @@ const Linear = ({
   item: {
     number,
     title,
-    isAngle,
-  }
+  },
+  isAngle
 }) => {
   const wrapper = useRef(null);
   const isInView = useInView(wrapper, { once: true, margin: "0px 0px -33% 0px" });
@@ -17,6 +17,7 @@ const Linear = ({
     <div className={styles.linear} data-angle={isAngle} ref={wrapper}>
       <div className={styles.bar}>
         <motion.div
+          className={styles.progressBar}
           initial={{ width: 0 }}
           animate={{ width: isInView ? `${number}%` : 0 }}
           exit={{ width: 0 }}
@@ -24,11 +25,29 @@ const Linear = ({
             type: "spring",
             damping: 35,
             duration: .5,
+            ...!isAngle && {
+              delay: .1
+            }
           }}
           data-visible={isInView}
         >
           <p className={styles.title}>{title}</p>
         </motion.div>
+        {!isAngle && (
+          <div className={styles.placeholder} data-visible={isInView}>
+            {Array.from({ length: 10 }).map((_, i) => (
+              <motion.div
+                key={i}
+                initial={{ scale: 0 }}
+                animate={{ scale: isInView ? 1 : 0 }}
+                exit={{ scale: 0 }}
+                transition={{
+                  delay: i * .02
+                }}
+              />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
