56c4278e18c785c1cea74d27f7809ff2eccc37be
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-01-10 15:41:26 +0100

    changed strategy for GTM Script

--- next/src/app/layout.js
+++ next/src/app/layout.js
@@ -42,12 +42,12 @@ export default function RootLayout({ children }) {
     <html lang={locale}>
       <head>
         <SchemaOrganization />
-        <Script id="gtm" strategy="afterInteractive">
-            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${process.env.GTM_ID}');`}
+        <Script id="gtm" strategy="lazyOnload">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${process.env.GTM_ID}');`}
         </Script>
       </head>
       <body className={`${Saira.className} ${IBMPlexSansHebrew.variable}`}>
