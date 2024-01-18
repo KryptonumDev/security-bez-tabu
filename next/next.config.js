96d0605789dab311f598c2e26a9992b8604a1ca6
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-01-18 21:38:35 +0100

    remove assetPrefix from next.config file

--- next/next.config.js
+++ next/next.config.js
@@ -1,13 +1,10 @@
 const path = require('path');
 
-const isProd = process.env.NODE_ENV === 'production'
-
 const nextConfig = {
   productionBrowserSourceMaps: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
-  assetPrefix: isProd ? 'https://wyzwanie-security.securitybeztabu.pl' : undefined,
   images: {
     dangerouslyAllowSVG: true,
     remotePatterns: [
