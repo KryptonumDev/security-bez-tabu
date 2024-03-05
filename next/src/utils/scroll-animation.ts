7085c56a69401cc261d83a3314271a679b048aa8
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-03-05 17:11:30 +0100

    changed scrollAnimation util

---
+++ next/src/utils/scroll-animation.ts
@@ -0,0 +1,13 @@
+const scrollAnimation = (isInView: boolean, delay = 0) => ({
+  initial: { opacity: 0, y: 20 },
+  animate: {
+    opacity: isInView ? 1 : 0,
+    y: isInView ? 0 : 20,
+  },
+  transition: {
+    duration: 0.5,
+    delay: delay,
+  },
+});
+
+export default scrollAnimation;
