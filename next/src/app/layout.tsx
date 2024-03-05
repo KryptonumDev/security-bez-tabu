ac0a74c300506fcf60ec73e43277568dea597ef7
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-03-05 17:15:09 +0100

    updated layout.tsx

--- next/src/app/layout.tsx
+++ next/src/app/layout.tsx
@@ -1,66 +1,42 @@
-import '@/global/global.scss';
-import localFont from 'next/font/local';
 import Script from 'next/script';
-import Header from '@/components/_global/Header';
-import Footer from '@/components/_global/Footer';
+import '@/global/global.scss';
+import { Saira, IBM_Plex_Sans_Hebrew } from '@/global/fonts';
+import { LOCALE, THEME_COLOR } from '@/global/constants';
 import SchemaOrganization from '@/global/Schema/Organization';
-import { locale } from '@/global/Seo';
-
-const Saira = localFont({
-  src: [
-    {
-      path: '../assets/fonts/Saira-Regular.woff2',
-      weight: '400',
-      style: 'normal',
-    },
-  ],
-  display: 'swap',
-  fallback: ['sans-serif'],
-});
-const IBMPlexSansHebrew = localFont({
-  src: [
-    {
-      path: '../assets/fonts/IBMPlexSansHebrew-Regular.woff2',
-      weight: '400',
-      style: 'normal',
-    },
-  ],
-  display: 'swap',
-  fallback: ['sans-serif'],
-  variable: '--ibm-plex',
-});
 
 export const viewport = {
-  themeColor: '#0D111F',
+  themeColor: THEME_COLOR,
 };
 
-// export const runtime = 'edge'
+const isProduction = process.env.NODE_ENV === 'production';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang={locale}>
+    <html lang={LOCALE}>
       <head>
-        <SchemaOrganization />
-        <Script
-          id='gtm'
-          strategy='lazyOnload'
-        >
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        {isProduction && (
+          <Script
+            id='gtm'
+            strategy='lazyOnload'
+          >
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','${process.env.GTM_ID}');`}
-        </Script>
+          </Script>
+        )}
+        <SchemaOrganization />
       </head>
-      <body className={`${Saira.className} ${IBMPlexSansHebrew.variable}`}>
-        <Header />
-        <main id='main'>{children}</main>
-        <Footer />
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
-          }}
-        />
+      <body className={`${Saira.className} ${IBM_Plex_Sans_Hebrew.variable}`}>
+        {children}
+        {isProduction && (
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+            }}
+          />
+        )}
       </body>
     </html>
   );
