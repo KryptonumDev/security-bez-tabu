d83b1473bf39f21640c2d6bc8835c3175214a48c
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-03-06 08:56:11 +0100

    added constants.ts to global

--- next/src/global/constants.ts
+++ next/src/global/constants.ts
@@ -1,7 +1,45 @@
-export const regex = {
-  email: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-  phone: /^(?:\+(?:\d{1,3}))?(?:[ -]?\(?\d{1,4}\)?[ -]?\d{1,5}[ -]?\d{1,5}[ -]?\d{1,6})\s*$/,
-}
-export const easing = [.79, .14, .15, .86];
+/**
+ * Global declaration of themeColor in HEX format.
+ * @constant
+ */
+export const THEME_COLOR: string = '#0D111F';
 
-export const domain = 'https://wyzwanie-security.securitybeztabu.pl';
+/**
+ * Global declaration of backgroundColor in HEX format.
+ * @constant
+ */
+export const BACKGROUND_COLOR: string = '#0D111F';
+
+/**
+ * Global declaration of page language.
+ * @constant
+ */
+export const LOCALE: string = 'pl';
+
+/**
+ * Global declaration of domain name of the website. Be aware of the protocol and www or non-www prefix.
+ * @constant
+ */
+export const DOMAIN: string = 'https://lp.securitybeztabu.pl';
+
+/**
+ * Global declaration of default title.
+ * @constant
+ */
+export const DEFAULT_TITLE: string = 'Security Bez Tabu';
+
+/**
+ * Global declaration of regex.
+ * @constant
+ */
+export const REGEX: { email: RegExp; phone: RegExp; string: RegExp } = {
+  email: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+  phone: /^(?:\+(?:\d{1,3}))?(?:[ -]?\(?\d{1,4}\)?[ -]?\d{1,5}[ -]?\d{1,5}[ -]?\d{1,6})$/,
+  string: /^(?!\s+$)(.*?)\s*$/,
+};
+
+/**
+ * Global declaration of easing.
+ * @constant
+ */
+export const EASING = [0.65, 0.05, 0.36, 1];
