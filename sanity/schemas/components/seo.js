9ace3de361cbde99c6de3fe5960f78f8a3d097fa
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-03-07 12:59:53 +0100

    updated seo componenet schema

--- sanity/schemas/components/seo.js
+++ sanity/schemas/components/seo.js
@@ -2,19 +2,28 @@ export default {
   name: "seo",
   title: "SEO",
   type: "object",
+  validation: Rule => Rule.required(),
   fields: [
     {
       name: 'title',
       type: 'string',
-      title: 'Title',
-      validation: Rule => Rule.max(70).warning('The title field should be no longer than 70 characters.')
+      title: 'Tytuł',
+      description: 'Tytuł strony, który jest ważny ze względów SEO. Pojawia się np. w pasku przeglądarki oraz w wyszukiwarkach internetowych.',
+      validation: Rule => [
+        Rule.max(70).warning('Pole nie powinno być dłuższe niż 70 znaków.'),
+        Rule.required(),
+      ]
     },
     {
       name: 'description',
       type: 'text',
+      title: 'Opis',
       rows: 4,
-      title: 'Description',
-      validation: Rule => Rule.max(165).warning('The description field should not be longer than 165 characters.')
+      description: 'Opis strony, który jest ważny ze względów SEO. Pojawia się np. w wyszukiwarkach internetowych oraz podczas udostępniania strony na mediach społecznościowych.',
+      validation: Rule => [
+        Rule.max(165).warning('Pole nie powinno być dłuższe niż 165 znaków.'),
+        Rule.required(),
+      ]
     },
   ]
 }
