99a2d93d374c00e28899d668422508804035e530
Shembou <sshembou@gmail.com>
2024-03-12 16:29:13 +0100

    removed console.log

--- next/src/components/_global/CourseDetails/CourseDetails.tsx
+++ next/src/components/_global/CourseDetails/CourseDetails.tsx
@@ -2,7 +2,6 @@ import styles from './CourseDetails.module.scss';
 import type { CourseDetailsTypes } from './CourseDetails.types';
 
 const CourseDetails = ({ heading, list }: CourseDetailsTypes) => {
-  console.log(heading, list);
   return <section className={styles['CourseDetails']}></section>;
 };
 
