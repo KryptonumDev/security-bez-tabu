b7d8b536f6f8d6a2d637c52556468fb7d6dc528c
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-03-15 09:35:41 +0100

    Fixed orphans word and edit eslint rule

--- next/src/components/_global/Newsletter/Form/_State.tsx
+++ next/src/components/_global/Newsletter/Form/_State.tsx
@@ -14,14 +14,14 @@ const State = ({ isSuccess, setStatus, StatusIcon }: StateTypes & StatusIconType
             <h3>
               {StatusIcon.Success} <span>Dziękujemy za zapis!</span>
             </h3>
-            <p>Od teraz będziemy informować Cię o aktualnościach z Security Bez Tabu!</p>
+            <p>Od teraz będziemy informować Cię o aktualnościach z Security Bez Tabu!</p>
           </>
         ) : (
           <>
             <h3>
               {StatusIcon.Error} <span>Nie udało się zapisać...</span>
             </h3>
-            <p>Podczas przesyłania informacji pojawił się problem z serwerem. Spróbuj ponownie!</p>
+            <p>Podczas przesyłania informacji pojawił się problem z serwerem. Spróbuj ponownie!</p>
             <Button
               type='button'
               onClick={() => setStatus({ sending: false, success: undefined })}
