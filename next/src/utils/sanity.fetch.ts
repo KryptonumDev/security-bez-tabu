7675fbe822a46f680db3be2d135e204a12247641
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-03-19 15:18:00 +0100

    updated sanity.fetch.ts

--- next/src/utils/sanity.fetch.ts
+++ next/src/utils/sanity.fetch.ts
@@ -8,11 +8,19 @@ const token = process.env.SANITY_API_TOKEN;
 const dataset = 'production';
 const apiVersion = '2024-03-11';
 
+const isDraftMode = requestAsyncStorage.getStore() ? draftMode().isEnabled : false;
+
+if (isDraftMode && !token) {
+  throw new Error('The `SANITY_API_TOKEN` environment variable is required.');
+}
+
 const client = createClient({
   projectId,
   dataset,
   apiVersion,
   useCdn: false,
+  perspective: isDraftMode ? 'previewDrafts' : 'published',
+  ...(isDraftMode && { token }),
 });
 
 /**
@@ -31,13 +39,7 @@ export default async function sanityFetch<QueryResponse>({
   tags?: string[];
   params?: QueryParams;
 }): Promise<QueryResponse> {
-  const isDraftMode = requestAsyncStorage.getStore() ? draftMode().isEnabled : false;
-  if (isDraftMode && !token) {
-    throw new Error('The `SANITY_API_TOKEN` environment variable is required.');
-  }
   return await client.fetch<QueryResponse>(query, params, {
-    perspective: isDraftMode ? 'previewDrafts' : 'published',
-    ...(isDraftMode && { token }),
     cache: isDraftMode || !tags ? 'no-cache' : 'default',
     next: {
       tags: tags,
