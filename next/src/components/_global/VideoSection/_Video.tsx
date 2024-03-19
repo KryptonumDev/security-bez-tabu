d055571335c4d4b2d14046766e12158c319953e5
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-03-19 14:01:23 +0100

    improved accessibility for VideoSection

--- next/src/components/_global/VideoSection/_Video.tsx
+++ next/src/components/_global/VideoSection/_Video.tsx
@@ -4,20 +4,34 @@ import type { VideoTypes } from './VideoSection.types';
 
 const Video = ({ url, PlayIcon, ...props }: VideoTypes) => {
   const video = useRef(null);
+  const [controls, setControls] = useState(false);
   const [isPlaying, setIsPlaying] = useState(false);
+
+  const playVideo = () => {
+    const videoElement = video.current;
+    if (!videoElement) return;
+    if (videoElement.paused) {
+      videoElement.play().then(() => setControls(true));
+    }
+  };
 
   return (
     <div
       {...props}
       data-playing={isPlaying}
+      data-controls={controls}
     >
+      <button
+        onClick={playVideo}
+        aria-label={isPlaying ? 'Zatrzymaj film' : 'Odtwórz film'}
+      />
+      {PlayIcon}
       <div className='borderLeft' />
       <div className='borderRight' />
-      {PlayIcon}
       <video
         ref={video}
         src={url}
-        controls={true}
+        controls={controls}
         autoPlay={false}
         loop={false}
         onPlay={() => setIsPlaying(true)}
