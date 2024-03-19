1eb6965400d62765a2700388dd76b38b41ee807e
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-03-19 12:33:02 +0100

    reverted sanity.fetch util code

--- next/src/utils/sanity.fetch.ts
+++ next/src/utils/sanity.fetch.ts
@@ -1,21 +1,34 @@
 'use server';
-import { draftMode } from 'next/headers';
 import { createClient, type QueryParams } from 'next-sanity';
 import { requestAsyncStorage } from 'next/dist/client/components/request-async-storage.external';
 
 const projectId = process.env.SANITY_PROJECT_ID;
 const token = process.env.SANITY_API_TOKEN;
 const dataset = 'production';
-const apiVersion = '2024-03-08';
+const apiVersion = '2024-03-11';
+
+const isDraftMode = !!requestAsyncStorage.getStore()?.draftMode.isEnabled;
+
+if (isDraftMode && !token) {
+  throw new Error('The `SANITY_API_TOKEN` environment variable is required.');
+}
 
 const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  perspective: 'published',
+  perspective: isDraftMode ? 'previewDrafts' : 'published',
+  ...(isDraftMode && { token }),
   useCdn: false,
 });
 
+/**
+ * Performs a Sanity query in GROQ for fetching data.
+ * @param {string} query - The GROQ query.
+ * @param {string[]} [tags] - Recommended. The tags for Next Caching.
+ * @param {QueryParams} [params={}] - Optional. Used to query dynamic pages, like blog posts.
+ * @returns {Promise<QueryResponse>} Returns a promise of the SEO object.
+ */
 export default async function sanityFetch<QueryResponse>({
   query,
   tags,
@@ -25,15 +38,7 @@ export default async function sanityFetch<QueryResponse>({
   tags?: string[];
   params?: QueryParams;
 }): Promise<QueryResponse> {
-  const isDraftMode = requestAsyncStorage.getStore() ? draftMode().isEnabled : false;
-  if (isDraftMode && !token) {
-    throw new Error('The `SANITY_API_TOKEN` environment variable is required.');
-  }
   return await client.fetch<QueryResponse>(query, params, {
-    ...(isDraftMode && {
-      token: token,
-      perspective: 'previewDrafts',
-    }),
     cache: isDraftMode || !tags ? 'no-cache' : 'default',
     next: {
       tags: tags,
